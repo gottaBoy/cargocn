c1820448f62135fcabeebf64deb7b427f9f88b16
gottaBoy <460632205@qq.com>
2017-02-22 11:48:05 +0800

    1

--- webportal/build/js/production.js
+++ webportal/build/js/production.js
@@ -22439,8 +22439,8 @@ BD = {
             $("#identity").find('div.text').animateOnScroll({
                 scrollDownEffect: 'fadeInRight'
             });
-            $("#personal").find('div.text').animateOnScroll({
-                scrollDownEffect: 'fadeInLeftBig'
+            $("#personal").find('div.barnav-page').animateOnScroll({
+                scrollDownEffect: 'zoomInDown'
             });
             $("#style").find('div.text').animateOnScroll({
                 scrollDownEffect: 'fadeInRight'
